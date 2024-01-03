3bdcf6b5f86344cbc44d366c427bf0075e972e4d
王惟澤 <wangweize@wangweizedeMacBook-Air.local>
2024-01-03 14:45:26 +0800

    auth complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Home() {
   return (
-    <div>
-      <Button>123</Button>
-    </div>
-  )
+    <Carousel className="h-screen">
+      <CarouselContent className="h-screen">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="flex justify-center items-center h-screen">123</div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-6" />
+      <CarouselNext className="absolute right-6" />
+    </Carousel>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily:{
+        "Gothic":['Nanum Gothic', 'sans-serif']
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",

213a5f04aff5fbd83b8fe11ed76a55b673e5691c
王惟澤 <wangweize@wangweizedeMacBook-Air.local>
2024-01-20 14:18:09 +0800

    make carouselComponent reuseable

--- components/CarouselComponent.tsx
+++ components/CarouselComponent.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   Carousel,
@@ -8,16 +8,16 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
-export default function CarouselComponent() {
+type CarouselComponentProps = {
+  children: React.ReactNode;
+};
+
+export default function CarouselComponent({
+  children,
+}: CarouselComponentProps) {
   return (
     <Carousel className="h-full">
-      <CarouselContent className="h-full">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="flex justify-center items-center h-full">123</div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
+      <CarouselContent className="h-full">{children}</CarouselContent>
       <CarouselPrevious className="absolute left-6" />
       <CarouselNext className="absolute right-6" />
     </Carousel>
